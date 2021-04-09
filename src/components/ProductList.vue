e23cffac004d02c86249e095cc651ceca4cbc835
Daniel Boyd <drboyd95@gmail.com>
2021-04-08 21:48:08 -0600

    First Commit

---
+++ src/components/ProductList.vue
@@ -0,0 +1,102 @@
+<template>
+<div class="wrapper">
+  <div class="products">
+    <div class="product" v-for="product in products" :key="product.id">
+      <div class="info">
+        <h1>{{product.name}}</h1>
+        <p>{{product.profession}}</p>
+      </div>
+
+      <div class="stats">
+        <h3>str {{product.str}}</h3>
+        <h3>dex {{product.dex}}</h3>
+        <h3>con {{product.con}}</h3>
+        <h3>int {{product.int}}</h3>
+        <h3>cha {{product.cha}}</h3>
+        <h3>wis {{product.wis}}</h3>
+      </div>
+      <div class="delete">
+          <form v-on:submit.prevent="delChar">
+              <button v-on:click='delChar("product_id" - 1)' class="delete">Delete</button>
+          </form>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: 'ProductList',
+  props: {
+    products: Array
+  },
+  methods: {
+    delChar(location) {
+        this.$root.$data.products.splice(location, 1);
+    },
+  }
+}
+</script>
+
+
+<style scoped>
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 24px;
+}
+
+.products {
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.product {
+  margin: 10px;
+  margin-top: 50px;
+  width: 200px;
+}
+
+.info {
+  background: #ff0000;
+  color: #000;
+  padding: 10px 30px;
+  height: 80px;
+
+  justify-content: center;
+}
+
+.info h1 {
+  font-size: 16px;
+}
+
+.info h2 {
+  font-size: 14px;
+}
+
+.info p {
+  margin: 0px;
+  font-size: 10px;
+}
+
+
+.stats {
+  display: inline;
+}
+
+button {
+  height: 50px;
+  background: #000;
+  color: white;
+  border: none;
+}
+
+.auto {
+  margin-left: auto;
+}
+</style>
---
+++ src/views/Browse.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="create">
+      <form v-on:submit.prevent="addChar">
+        <input type="text" v-model="name" placeholder="Enter a name" />
+        <br /><br />
+        <p>Pick a class</p>
+        <select id="professions-list" v-model="profession">
+          <option value="Wizard">Wizard</option>
+          <option value="Rogue">Rogue</option>
+          <option value="Warrior">Warrior</option>
+          <option value="Bard">Warrior</option>
+        </select>
+        <br /><br />
+        <p>Enter a number between 8-20</p>
+        <input type="number" v-model="str" placeholder="strength" />
+        <input type="number" v-model="dex" placeholder="dexterity" />
+        <input type="number" v-model="con" placeholder="constitution" />
+        <input type="number" v-model="int" placeholder="intelligence" />
+        <input type="number" v-model="cha" placeholder="charisma" />
+        <input type="number" v-model="wis" placeholder="wisdom" />
+        <br /><br />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default { 
+  data() {
+    return {
+      name: "",
+      profession: "",
+      str: "",
+      dex: "",
+      con: "",
+      int: "",
+      cha: "",
+      wis: "",
+    };
+  },
+  methods: {
+    addChar() {
+      this.$root.$data.products.push({
+        name: this.name,
+        profession: this.profession,
+        str: this.str,
+        dex: this.dex,
+        con: this.con,
+        int: this.int,
+        cha: this.cha,
+        wis: this.wis,
+      });
+      console.log(this.name),
+        (this.name = ""),
+        (this.str = ""),
+        (this.dex = ""),
+        (this.con = ""),
+        (this.int = ""),
+        (this.cha = ""),
+        (this.wis = "");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.create {
+  display: flex;
+  justify-content: center;
+}
+</style>
